79e32a3c35e965727694887328f2383ab92f764d
Mohd Iqbal <mohdiqbalradzuan@gmail.com>
2021-07-18 17:57:20 +0800

    1. Increase version to 1.0.3 2. Include boot service - run at startup 3. Refactoring

---
+++ android/app/src/main/kotlin/com/solat_tv/solat_tv/SolatTvServiceAtBootReceiver.kt
@@ -0,0 +1,18 @@
+package com.solat_tv.solat_tv
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SolatTvServiceAtBootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            val thisIntent = Intent(context, MainActivity::class.java)
+            thisIntent.setAction("android.intent.action.MAIN")
+            thisIntent.addCategory("android.intent.category.LAUNCHER")
+            thisIntent.addCategory("android.intent.category.LEANBACK_LAUNCHER")
+            thisIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(thisIntent)
+        }
+    }
+}
